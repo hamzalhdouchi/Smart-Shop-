08d1e55418ae6fde8e3b10375301f21bd5ac4311
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-27 09:44:34 +0100

    SMR-42 feat: add InsufficientStockException class for handling insufficient stock errors

---
+++ src/main/java/com/smartshop/exception/InsufficientStockException.java
@@ -0,0 +1,10 @@
+package com.smartshop.exception;
+
+
+public class InsufficientStockException extends RuntimeException {
+
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+}
+
