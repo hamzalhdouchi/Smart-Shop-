f175dfc4f218bf3b95f10ab1bb6636fb44084d17
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-27 09:42:32 +0100

    SMR-22 feat: add ProductResponseDTO for product response data representation

---
+++ src/main/java/com/smartshop/dto/response/product/ProductResponseDTO.java
@@ -0,0 +1,21 @@
+package com.smartshop.dto.response.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductResponseDTO {
+
+    private String id;
+    private String nom;
+    private BigDecimal prixUnitaire;
+    private BigDecimal stockDisponible;
+}
+
