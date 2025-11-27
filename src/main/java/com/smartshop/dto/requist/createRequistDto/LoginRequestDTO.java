0a0810fb1cf503e75fc342f328dce6fb78001aba
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-27 09:48:55 +0100

    SMR-27 feat: add LoginRequestDTO for managing user login details

---
+++ src/main/java/com/smartshop/dto/requist/createRequistDto/LoginRequestDTO.java
@@ -0,0 +1,11 @@
+package com.smartshop.dto.requist.createRequistDto;
+
+
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+    private String username;
+    private String password;
+}
+
