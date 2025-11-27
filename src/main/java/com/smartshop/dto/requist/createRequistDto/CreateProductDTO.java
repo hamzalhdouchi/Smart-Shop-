c9d817a4e1162bf952639041c3ce3c6e606e0d43
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-27 09:48:19 +0100

    SMR-27 feat: add CreateProductDTO for managing product creation details

---
+++ src/main/java/com/smartshop/dto/requist/createRequistDto/CreateProductDTO.java
@@ -0,0 +1,31 @@
+package com.smartshop.dto.requist.createRequistDto;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CreateProductDTO {
+
+    @NotBlank(message = "Product name is required")
+    @Size(min = 2, max = 100, message = "Product name must be between 2 and 100 characters")
+    private String nom;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.01", message = "Price must be greater than 0")
+    @Digits(integer = 10, fraction = 2, message = "Price must have maximum 10 integer digits and 2 decimal digits")
+    private BigDecimal prixUnitaire;
+
+    @DecimalMin(value = "0", message = "Stock must be 0 or greater")
+    @Digits(integer = 10, fraction = 2, message = "Stock must have maximum 10 integer digits and 2 decimal digits")
+    @Builder.Default
+    private BigDecimal stockDisponible = BigDecimal.ZERO;
+}
+
