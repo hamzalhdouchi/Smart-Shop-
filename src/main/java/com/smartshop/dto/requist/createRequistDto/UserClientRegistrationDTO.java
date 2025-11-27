a66ec70eec2ef842bdcef874cdddc39c8939b324
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-27 09:33:46 +0100

    SMR-2 feat: add UserClientRegistrationDTO for user registration with validation

---
+++ src/main/java/com/smartshop/dto/requist/createRequistDto/UserClientRegistrationDTO.java
@@ -0,0 +1,40 @@
+package com.smartshop.dto.requist.createRequistDto;
+import com.smartshop.enums.UserRole;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserClientRegistrationDTO {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9._-]+$",
+            message = "Username can only contain letters, numbers, dots, underscores and hyphens")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$",
+            message = "Password must contain at least one digit, one lowercase, one uppercase, and one special character")
+    private String password;
+
+    @NotNull(message = "User role is required")
+    private UserRole role;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
+    private String nom;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Size(max = 100, message = "Email must not exceed 100 characters")
+    private String email;
+}
+
+
