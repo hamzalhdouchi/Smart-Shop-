4f583984782ebc34c74b0a5c935e6f8daa3b1586
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-27 09:44:05 +0100

    SMR-42 feat: add DuplicateResourceException class for handling duplicate resource errors

---
+++ src/main/java/com/smartshop/exception/DuplicateResourceException.java
@@ -0,0 +1,10 @@
+package com.smartshop.exception;
+
+
+public class DuplicateResourceException extends RuntimeException {
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+
+}
