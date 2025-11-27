2ea53bc9f7183e5154d384277f444ee46940b6ac
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-27 09:38:24 +0100

    SMR-7 feat: add ClientResponseDTO for client response data representation

---
+++ src/main/java/com/smartshop/dto/response/client/ClientResponseDTO.java
@@ -0,0 +1,23 @@
+package com.smartshop.dto.response.client;
+
+import com.smartshop.enums.ClientTier;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClientResponseDTO {
+    private String id;
+    private String nom;
+    private String email;
+    private ClientTier tier;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
+
