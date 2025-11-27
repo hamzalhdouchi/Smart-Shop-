6c2f977913e46d5629988416d10cabf406b1eefd
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-27 09:41:24 +0100

    SMR-22 feat: add ProductAdvancedResponseDTO for detailed product response data

---
+++ src/main/java/com/smartshop/dto/response/product/ProductAdvancedResponseDTO.java
@@ -0,0 +1,26 @@
+package com.smartshop.dto.response.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductAdvancedResponseDTO {
+
+    private String id;
+    private String nom;
+    private BigDecimal prixUnitaire;
+    private BigDecimal stockDisponible;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private boolean inStock;
+    private String stockStatus;
+}
+
