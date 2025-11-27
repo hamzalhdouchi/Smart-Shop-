287a62c7a8ce4e73cb65f543c806b8d0c72029ae
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-27 09:38:12 +0100

    SMR-7 feat: add ClientRepository for client data access and management

---
+++ src/main/java/com/smartshop/repository/ClientRepository.java
@@ -0,0 +1,14 @@
+package com.smartshop.repository;
+
+import com.smartshop.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, String> {
+    boolean existsByEmail(String email);
+    Optional<Client> findByEmail(String email);
+}
+
