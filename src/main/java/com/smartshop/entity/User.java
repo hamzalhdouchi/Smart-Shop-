97970947af4ea142aa622dcd2fcc61343731a344
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-27 09:33:28 +0100

    SMR-2 feat: add User entity with attributes and relationships

---
+++ src/main/java/com/smartshop/entity/User.java
@@ -0,0 +1,40 @@
+package com.smartshop.entity;
+
+import com.smartshop.audit.Auditable;
+import com.smartshop.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Client client;
+}
+
