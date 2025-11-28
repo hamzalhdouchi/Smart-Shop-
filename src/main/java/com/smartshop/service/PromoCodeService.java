43d2d76499160468d2be4dde20abd583b6c9ff07
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-28 21:41:45 +0100

    SMR-16 feat: add PromoCodeService interface for managing promotional code operations

---
+++ src/main/java/com/smartshop/service/PromoCodeService.java
@@ -0,0 +1,17 @@
+package com.smartshop.service;
+
+
+
+import com.smartshop.dto.requist.createRequistDto.PromoCodeCreateDTO;
+import com.smartshop.dto.response.promoCode.PromoCodeResponseDTO;
+
+import java.util.List;
+
+public interface PromoCodeService {
+    PromoCodeResponseDTO createPromoCode(PromoCodeCreateDTO dto);
+    PromoCodeResponseDTO getPromoCodeByCode(String code);
+    List<PromoCodeResponseDTO> getAllPromoCodes();
+    boolean isPromoCodeAvailable(String code);
+    void markAsUsed(String code);
+    void activatePromoCode(String code);
+}
