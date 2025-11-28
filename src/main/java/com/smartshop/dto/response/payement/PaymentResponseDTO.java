3c3fa395fe0d4b40225244d24415e065deb04a27
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-28 21:35:06 +0100

    SMR-36 feat: add PaymentResponseDTO for handling payment response data

---
+++ src/main/java/com/smartshop/dto/response/payement/PaymentResponseDTO.java
@@ -0,0 +1,26 @@
+package com.smartshop.dto.response.payement;
+
+import com.smartshop.enums.PaymentStatus;
+import com.smartshop.enums.PaymentType;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaymentResponseDTO {
+
+    private String id;
+    private String orderId;
+    private Integer paymentNumber;
+    private String reference;
+    private BigDecimal montant;
+    private LocalDate datePayment;
+    private LocalDate dateEcheance;
+    private PaymentType typePayment;
+    private PaymentStatus paymentStatus;
+}
+
