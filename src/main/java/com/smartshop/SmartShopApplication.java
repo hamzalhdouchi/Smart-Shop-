27a55ce09a77f67fa17c72dbd4e648a6535dd82b
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-28 21:46:34 +0100

    refactor: improve server startup message and clean up commented code

--- src/main/java/com/smartshop/SmartShopApplication.java
+++ src/main/java/com/smartshop/SmartShopApplication.java
@@ -1,26 +1,45 @@
 package com.smartshop;
 
+import com.smartshop.dto.requist.createRequistDto.UserClientRegistrationDTO;
 import com.smartshop.entity.User;
 import com.smartshop.enums.UserRole;
+import com.smartshop.mapper.ClientMapper;
 import com.smartshop.repository.UserRepository;
+import com.smartshop.service.ClientService;
+import com.smartshop.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+//@RequiredArgsConstructor
 public class SmartShopApplication {
+
+//    private final ClientService userRepository;
+//    private final ClientMapper userService;
 
     public static void main(String[] args) {
         SpringApplication.run(SmartShopApplication.class, args);
 
         System.out.println("||========================================||");
-        System.out.println("||      Server is work Seccessfuly        ||");
+        System.out.println("||      Server is working successfully    ||");
         System.out.println("||========================================||");
     }
 
-
+//    @Bean
+//    CommandLineRunner initDatabase() {
+//        return args -> {
+//            UserClientRegistrationDTO userClientRegistrationDTO = new UserClientRegistrationDTO();
+//            userClientRegistrationDTO.setUsername("admin");
+//            userClientRegistrationDTO.setPassword("admin");
+//            userClientRegistrationDTO.setRole(UserRole.ADMIN);
+//            userClientRegistrationDTO.setNom("admindd");
+//            userClientRegistrationDTO.setEmail("admindd@gmail.com");
+//            userRepository.create(userClientRegistrationDTO);
+//        };
+//    }
 }
