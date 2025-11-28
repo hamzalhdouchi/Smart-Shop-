651dddb02673c936c1f8221b236669bc8873788f
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-28 21:33:48 +0100

    SMR-36 refactor: rename PaiementStatus to PaymentStatus and update Payment entity fields for consistency

--- src/main/java/com/smartshop/entity/Payment.java
+++ src/main/java/com/smartshop/entity/Payment.java
@@ -1,57 +1,39 @@
 package com.smartshop.entity;
 
 import com.smartshop.audit.Auditable;
-import com.smartshop.enums.PaiementStatus;
+import com.smartshop.enums.PaymentStatus;
 import com.smartshop.enums.PaymentType;
 import jakarta.persistence.*;
 import lombok.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "paiements")
-@Data
+@Table(name = "payments")
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Payment extends Auditable {
-
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "commande_id", nullable = false)
-    private Order order;
-
-    @Column(name = "numero_paiement", nullable = false)
-    private Integer numeroPaiement;
-
-    @Column(nullable = false, precision = 10, scale = 2)
+    private Integer paymentNumber;
+    private String reference;
     private BigDecimal montant;
+    private LocalDate datePayment = LocalDate.now();
+    private LocalDate dateEcheance;;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "type_paiement", nullable = false, length = 20)
-    private PaymentType typePaiement;
+    private PaymentType typePayment;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "type_status", nullable = false, length = 20)
-    private PaiementStatus paiementStatus;
+    private PaymentStatus paymentStatus;
 
-
-    @Column(name = "reference_paiement", length = 100)
-    private String referencePaiement;
-
-    @Column(length = 100)
-    private String banque;
-
-    @Column(name = "date_paiement", nullable = false)
-    private LocalDate datePaiement;
-    @Column(name = "date_echeance")
-    private LocalDate dateEcheance;
-
-    @Column(name = "date_encaissement")
-    private LocalDate dateEncaissement;
-
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id")
+    private Order order;
 }
-
