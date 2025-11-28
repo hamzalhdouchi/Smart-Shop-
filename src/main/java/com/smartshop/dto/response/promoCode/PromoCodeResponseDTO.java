1785101d72effb35a65de3e98c40cd5e2dcbdc51
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-28 21:41:30 +0100

    SMR-16 feat: add PromoCodeResponseDTO for representing promotional code responses

---
+++ src/main/java/com/smartshop/dto/response/promoCode/PromoCodeResponseDTO.java
@@ -0,0 +1,17 @@
+package com.smartshop.dto.response.promoCode;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PromoCodeResponseDTO {
+    private String id;
+    private String code;
+    private BigDecimal remisePourcentage;
+    private Boolean disponible;
+}
+
