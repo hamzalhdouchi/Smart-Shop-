59d7ab01b717241407d813257d9d911f97366e3e
hamzalhadouchi <hamzabniss16@gmail.com>
2025-11-29 22:10:15 +0100

    SMR-7 feat: add ClientStatisticsDTO for client statistics response

---
+++ src/main/java/com/smartshop/dto/response/client/ClientStatisticsDTO.java
@@ -0,0 +1,24 @@
+package com.smartshop.dto.response.client;
+
+import com.smartshop.enums.ClientTier;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientStatisticsDTO {
+    private String clientId;
+    private String nom;
+    private String email;
+    private ClientTier tier;
+    private Integer totalOrders;
+    private BigDecimal totalSpent;
+
+}
